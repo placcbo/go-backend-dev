08e2d8103fa0bf925775825b099f76eeb6c28457
Kevin Ndirangu <placbo2@gmail.com>
2026-03-05 13:10:12 +0300

    ...

--- week2-structs-interfaces/day7-methods-receivers/main.go
+++ week2-structs-interfaces/day7-methods-receivers/main.go
@@ -1,94 +1,27 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
+import "fmt"
 
-type Company struct {
-	Name     string
-	Location Address
+type BankAccount struct {
+	Owner   string
+	Balance float64
 }
 
-type Address struct {
-	City    string
-	Country string
-}
-type JobApplication struct {
-	ID        int     `json:"id"`
-	Role      string  `json:"role"`
-	Company   Company `json:"company"`
-	SalaryUSD int     `json:"salaryusd"`
-	Status    string  `json:"status"`
-	AppliedAt string  `json:"appliedat,omitempty"`
-	IsRemote  bool    `json:"isremote"`
+func (d *BankAccount) Deposit(amount float64) {
+	if amount <= 0 {
+		fmt.Println("Deposit money cant be zero or less")
+	}
+	d.Balance += amount
+
 }
 
 func main() {
-	applications := []JobApplication{
-		{
-			ID:   1,
-			Role: "backend dev",
-			Company: Company{
-				Name: "Cloudfactory",
-				Location: Address{
-					Country: "kenya",
-					City:    "Nairobi",
-				},
-			},
-			SalaryUSD: 300,
-			Status:    "pending",
-			AppliedAt: "",
-			IsRemote:  true,
-		},
-		{
-			ID:   2,
-			Role: "QA engineer",
-			Company: Company{
-				Name: "nuvita",
-				Location: Address{
-					Country: "Uganda",
-					City:    "kamplala",
-				},
-			},
-			SalaryUSD: 500,
-			Status:    "pending",
-			AppliedAt: "11/11/2023",
-			IsRemote:  false,
-		},
-		{
-			ID:   3,
-			Role: "backend engineer",
-			Company: Company{
-				Name: "youtube",
-				Location: Address{
-					Country: "rwanda",
-					City:    "Kigali",
-				},
-			},
-			SalaryUSD: 300,
-			Status:    "rejected",
-			AppliedAt: "2025-11-09",
-			IsRemote:  false,
-		},
+	kevin := BankAccount{
+		Owner:   "Kevin",
+		Balance: 250.0,
 	}
-	data, err := json.MarshalIndent(applications, "", "")
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-	fmt.Println(string(data))
 
-	filterByPending := filterByStatus(applications, "rejected")
-	fmt.Println("filter by pending: ", filterByPending)
-}
+	kevin.Deposit(600)
+	fmt.Println(kevin)
 
-func filterByStatus(apps []JobApplication, status string) []JobApplication {
-	results := make([]JobApplication, 0)
-	for _, app := range apps {
-		if app.Status == status {
-			results = append(results, app)
-		}
-
-	}
-	return results
 }
